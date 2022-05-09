4299ce4a3bdd3408d3ef0e10076e077fb77e6866
urlGuney <guneyural@gmail.com>
2022-05-09 13:42:37 +0300

    initial commit

---
+++ src/app/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import reservationsReducer from "../features/reservationSlice";
+import customerReducer from "../features/customerSlice";
+
+export const store = configureStore({
+  reducer: { reservations: reservationsReducer, customers: customerReducer },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
